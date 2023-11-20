fdea3155466569ae97106cf5a812733d4e3bc008
sebastian <sebastian.hartwig@uni-ulm.de>
2023-11-20 20:44:04 +0100

    update transformers requ

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         packages=find_packages(exclude=["tests*"]),
         install_requires=[
             'timm',
-            'transformers>=4.27.4',
+            'transformers>=4.27',
             'fairscale==0.4.13',
             'huggingface_hub>=0.13.4',
             'diffusers>=0.16.0',
